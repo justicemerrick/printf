b091c6f7c90b0eb8a20c6dfe52670785fbdc26f2
Jennifer <Iruomaonyia@outlook.com>
2022-08-08 19:26:24 +0100

    Initial version of print_s and print_d

--- main.h
+++ main.h
@@ -11,6 +11,11 @@ typedef struct specifier
 }spec_t;
 int specifier_check(char, va_list arg);
 int print_c(va_list arg);
+int print_s(va_list arg);
+int print_d(va_list);
+int print_b(va_list);
+char *convert_number(int64_t num, int base);
+int _pow(int, int);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
--- test/main.h
+++ test/main.h
@@ -9,8 +9,13 @@ typedef struct specifier
 	char c;
 	int (*f)(va_list);
 }spec_t;
-int specifier_check(char , va_list arg);
+int specifier_check(char, va_list arg);
 int print_c(va_list arg);
+int print_s(va_list arg);
+int print_d(va_list);
+int print_b(va_list);
+char *convert_number(int64_t num, int base);
+int _pow(int, int);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
